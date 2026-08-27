95b2808d3a0b1fda730aafe9edf0f5189706f806
Vonkhirby <khirbyvooon13@gmail.com>
2026-08-27 15:29:21 +0800

    Assignment_2

---
+++ Assignment_2/activity_2(2).php
@@ -0,0 +1,63 @@
+<?php 
+    if(isset($_GET['name'])){
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $phoneNum = $_GET['phoneNum'];
+        $address = $_GET['address'];
+        $prog = $_GET['prog'];
+        $parName = $_GET['parName'];
+        $parNum = $_GET['parNum'];
+        $careerObj = $_GET['careerObj'];
+        $skills = $_GET['skills'];
+        $experience = $_GET['experience'];
+?>
+    <table style="width:100%; border: 20px solid #c7b700; border-collapse: collapse; color: #004a91">
+        <tr>
+            <td colspan="2" style="background-color: #004a91; padding: 50px; text-align:center">
+                <h1 style="color: white; margin: 0;">Submitted Information</h1>
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold; width: 30%;">Name: </td>
+            <td style="padding: 8px;"><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Email: </td>
+            <td style="padding: 8px;"><?php echo $email; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">PhoneNumber: </td>
+            <td style="padding: 8px;"><?php echo $phoneNum; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Address: </td>
+            <td style="padding: 8px;"><?php echo $address; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Parent Name: </td>
+            <td style="padding: 8px;"><?php echo $parName; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Parent Number: </td>
+            <td style="padding: 8px;"><?php echo $parNum; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Program: </td>
+            <td style="padding: 8px;"><?php echo $prog; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Career Objectives: </td>
+            <td style="padding: 8px;"><?php echo $careerObj; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Key Skills: </td>
+            <td style="padding: 8px;"><?php echo $skills; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Experience: </td>
+            <td style="padding: 8px;"><?php echo $experience; ?></td>
+        </tr>
+    </table>
+<?php 
+    }
+?>
---
+++ Assignment_2/activity_2.php
@@ -0,0 +1,52 @@
+<form action="activity_2(2).php" method="get">
+    <table style="width:100%; border: 20px solid #c7b700; border-collapse: collapse; color: #004a91">
+        <tr>
+            <td colspan="2" style="background-color: #004a91; padding: 50px; text-align:center">
+                <h1 style="color: white; margin: 0;">Resume</h1>
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold; width: 30%;">Name: </td>
+            <td><input type="text" name="name" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Email: </td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">PhoneNumber: </td>
+            <td><input type="number" name="phoneNum" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Address: </td>
+            <td><input type="text" name="address" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Parent Name: </td>
+            <td><input type="text" name="parName" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Parent Number: </td>
+            <td><input type="number" name="parNum" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Program: </td>
+            <td><input type="text" name="prog" ></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Career Objectives: </td>
+            <td><textarea name="careerObj" rows="3" style="width: 100%;"></textarea></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Key Skills: </td>
+            <td><textarea name="skills" rows="3" style="width: 100%;"></textarea></td>
+        </tr>
+        <tr>
+            <td style="background-color: #d3d3d3; padding: 8px; font-weight: bold;">Experience: </td>
+            <td><textarea name="experience" rows="3" style="width: 100%;"></textarea></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding: 8px; font-weight: bold; text-align: center;"><button type="submit">Submit</button></td>
+        </tr>
+    </table>
+</form>
